1921cef0dbff67be9236c5ce0b3c978e4a20bb7a
Psoloid <soloid@mail.ru>
2015-09-17 21:19:17 +0300

    Hello World commit

---
+++ Hillel/src/MyFirstClass.java
@@ -0,0 +1,17 @@
+/**
+ * Created by NewClass8 on 17.09.2015.
+ */
+public class MyFirstClass {
+        public static void main(String[] args) {
+                String helloMessage = "Hello World";
+                System.out.println(helloMessage);
+
+                int myBirthYear = 1986
+
+                int age = 28;
+                System.out.println("I'm " + age);
+
+                System.out.println(31 + 31);
+
+        }
+}
