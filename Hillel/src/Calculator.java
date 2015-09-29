3ab6a587dc024864e2461588697c03347e7f66da
MAgameriants <mahaso@rambler.ru>
2015-09-29 23:57:55 +0300

    AboutMyself Class is added

--- Hillel/src/Calculator.java
+++ Hillel/src/Calculator.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        int firstNumber;
+        double firstNumber;
         System.out.println("Enter the first number");
         Scanner in = new Scanner(System.in);
-        firstNumber = in.nextInt();
-        int secondNumber;
+        firstNumber = in.nextDouble();
+        double secondNumber;
         System.out.println("Enter the second number");
-        secondNumber = in.nextInt();
+        secondNumber = in.nextDouble();
         String operation;
         System.out.println("Enter operation");
         operation = in.next();
@@ -29,13 +29,34 @@ public class Calculator {
             case "/":
                 System.out.println("Result: " + (firstNumber / secondNumber));
                 break;
-
         }
+
+        double thirdNumber;
+        System.out.println("Enter third number");
+        thirdNumber = in.nextDouble();
+        String operation2;
+        System.out.println("Enter operation with the double");
+        operation2 = in.next();
+
+        switch (operation2) {
+            case "trunk":
+                double d1;
+                d1 = Math.floor(thirdNumber);
+                int d2 = (int) d1;
+                System.out.println("Result: " + d2);
+                break;
+            case "fract":
+                d1 = Math.floor(thirdNumber);
+                System.out.println("Result: " + (thirdNumber - d1));
+                break;
+        }
+
     }
 }
 
 
 /**
+ * byte, short, int, long или double
  * 1) ввести число
  * 2) ввести операцию
  * 3) ввести еще одно число
