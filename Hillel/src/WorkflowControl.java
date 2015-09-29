b3304a7a0c844e47bce0712abae4737e7afffe5f
MAgameriants <mahaso@rambler.ru>
2015-09-29 22:46:01 +0300

    AboutMyself Class is added

--- Hillel/src/WorkflowControl.java
+++ Hillel/src/WorkflowControl.java
@@ -5,6 +5,79 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  */
 public class WorkflowControl {
     public static void main(String[] args) {
+
+        byte b =120;
+        short s = b;
+        long l = s;
+
+        double c = l; /*потеря точности может произойти */
+
+        l = 15;
+        int z = (int) l; /*отрежет самые старшие и самые значащие биты */
+
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println("Int Max value:" + maxValue);
+        long intMaxValue = maxValue;
+        long intOverflow = intMaxValue + 1;
+        int i = (int) intOverflow;
+
+        double d = Math.PI;
+        i =  (int) d;
+
+        System.out.println("PI: " + i);
+
+        System.out.println("Overflowed ubt: " +i);
+
+
+        switchCase2();
+
+
+        /*int number = 5;
+
+        int isEven = isEven(number);
+
+        isEven = (number % 2 == 0) ? "yes" : "no";  /*тернарный оператор
+
+    }
+
+    public static boolean isEven(int someNumber){
+        if(someNumber % 2 == 0) {
+            return true;
+        } else {{
+            return false;
+        }}
+    */
+    }
+
+    private static void switchCase2() {
+        int month = 2;
+        switch (month){
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("winter");
+                break;
+            case 3:
+            case 4:
+                System.out.println("spring");
+                break;
+        }
+    }
+
+    private static void switchCaseExample(){
+            String month = "Aug";
+            switch (month) {
+                case "Jan":
+                    System.out.println("01");
+                    break;
+                case "May":
+                    System.out.println("05");
+                    break;
+                default:
+                    System.out.println("Testing default");
+                    break;
+            };
+
         int age = 25;
 
         if (age < 10) {
@@ -18,9 +91,6 @@ public class WorkflowControl {
         }
     }
 
-
-
-
 public static void ifExplanation(){
         boolean condition = false;
         if(condition){
@@ -33,5 +103,3 @@ public static void ifExplanation(){
     }
 
 }
-
-
